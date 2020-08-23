0ce16b57dda6bd218417eba6d6561ceb954f68d6
Thomaz Henrique <treimberg7@gmail.com>
2020-08-23 08:29:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const bot = new Discord.Client();
 bot.login('NzQ3MDE4NjIzOTk3OTY4NDg4.X0IxTg.EeNcMT2-TbxFQlFZr5ET1Vu8dPY');
 
 bot.on('message', message => {
+    let botName = bot.user.username.toString();
+    console.log(bot.user.username);
     let responseObject = {
-        "bom dia" : "Bom dia!",
-        "ba dia" : "Ba diaa!"
+        [`bom dia @${botName}`] : "Bom dia!",
+        [`bom dia! ${botName}`] : "Bom dia!!",
+        [`ba dia ${bot.}`] : "Ba diaa!",
+        [`ba dia! ${botName}`]: "Ba diaa!!"
     };
 
     message.content = message.content.toLowerCase();
-    
+
     if (responseObject[message.content]) {
         message.channel.send(responseObject[message.content]);
     }
