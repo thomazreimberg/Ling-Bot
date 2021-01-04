3b4e4276261ee6172ff3062267ac96a61c8f4d57
Thomaz Henrique <57465943+thomazreimberg@users.noreply.github.com>
2021-01-04 07:26:37 -0300

    logs

--- indextt.js
+++ indextt.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // Define configuration options
 const opts = {
     options: {
-        debug: false,
+        debug: true,
         messagesLogLevel: "info"
     },
 
@@ -43,19 +43,17 @@ module.exports = {
         // Called every time a message comes in
         function onMessageHandler (channel, tags, message, self) {
             if (self) { return; } // Ignore messages from the bot
-            if (tags.username === 'lingming_'){
-                return
-            }
+
             // Remove whitespace from chat message
             const commandName = message.trim().toLowerCase();
             // If the command is known, let's execute it
-            if (commandName == 'boa noite' || commandName == 'ba noite' || commandName == 'noite') {
+            if (commandName == 'boa noite' || commandName == 'ba noite' || commandName == 'noite' && tags.username !== 'lingming_') {
                 //client.say(channel, 'boa noite');
                 console.log('ba noite');
-            } else if (commandName == 'boa tarde' || commandName == 'ba tarde' || commandName == 'tarde') {
+            } else if (commandName == 'boa tarde' || commandName == 'ba tarde' || commandName == 'tarde' && tags.username !== 'lingming_') {
                 //client.say(channel, 'ba tarde');
                 console.log('ba tarde');
-            } else if (commandName == 'bom dia' || commandName == 'ba dia' || commandName == 'dia') {
+            } else if (commandName == 'bom dia' || commandName == 'ba dia' || commandName == 'dia' && tags.username !== 'lingming_') {
                 //client.say(channel, 'ba dia');
                 console.log('ba dia');
             } else {
